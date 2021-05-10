54756136fbd32b64ebe531d7b994234e33c3deac
Ritaban Datta <ritabandatta2000@gmail.com>
2021-05-11 00:05:51 +0530

    n odd nuum

---
+++ printnoddnum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int n, i = 1;
+    printf("enter a number:");
+    scanf("%d", &n);
+    do
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d\n", i);
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    } while (i <= 2 * n);
+    return 0;
+}
