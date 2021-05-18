875047476c19f5dff1d0e72d09458d0118c49b8a
Ritaban Datta <ritabandatta2000@gmail.com>
2021-05-19 05:07:35 +0530

    array

---
+++ 2darray.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i, j, n, m, sum = 0;
+    printf("enter the number of rows and coloumn:");
+    scanf("%d %d", &i, &j);
+    int a[i][j];
+    printf("enter the elements of the %d * %d matrix", i, j);
+    for (n = 0; n < i; n++)
+    {
+        for (m = 0; m < j; m++)
+        {
+            printf("enter the %d %d element:", n, m);
+            scanf("%d", &a[n][m]);
+        }
+    };
+
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + a[i][i];
+    }
+    printf("%d", sum);
+    return 0;
+}
