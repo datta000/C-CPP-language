68ecc39378b8c7791367cbad93d44c383202c2b1
Ritaban Datta <ritabandatta2000@gmail.com>
2021-09-09 09:36:45 +0530

    traverse

--- traverse.cpp
+++ traverse.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
     int i,j,n;
-    int arr[20];
-    // cout<<"enter the size of the array";
-    // cin>>n;
+    cout<<"enter the size of the array";
+    cin>>n;
+    int arr[n];
     cout<<"enter the elements of the array";
-    for(i=0;i<20;i++){
+    for(i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(j=0;j<20;j++){
-        cout<<arr[j]2;
+    for(j=0;j<n;j++){
+        cout<<arr[j];
     }
     return 0;
 }
