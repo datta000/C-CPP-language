21edf0bce8777d52941f9f46fccc48b6b2493cf2
Ritaban Datta <ritabandatta2000@gmail.com>
2021-09-10 10:11:07 +0530

    insert in array

--- traverse.cpp
+++ traverse.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 int main(){
     int i,j,n;
-    cout<<"enter the size of the array";
+    
+    cout<<"enter the size of the array: ";
     cin>>n;
     int arr[n];
-    cout<<"enter the elements of the array";
+    cout<<"enter the elements of the array: ";
     for(i=0;i<n;i++){
         cin>>arr[i];
     }
+    cout<<"\n";
     for(j=0;j<n;j++){
-        cout<<arr[j];
+        cout<<arr[j]<<" ";
     }
     return 0;
 }
