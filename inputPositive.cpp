21818b82afd43bc13d69d2231a81a4ee6345b72a
Ritaban Datta <ritabandatta2000@gmail.com>
2021-09-12 01:51:22 +0530

    skip 3X

--- inputPositive.cpp
+++ inputPositive.cpp
@@ -1,1 +1,15 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter a positive number";
+    cin >> n;
+    while (n < 0)
+    {
+        cout << "enter a positive number";
+        cin >> n;
+    }
+    cout << n;
+    return 0;
+}
